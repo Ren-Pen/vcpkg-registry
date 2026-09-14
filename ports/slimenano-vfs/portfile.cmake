c0594eb7ae7c510928af4b2c0aed61ca8d250ddd
Ren-Pen <xzy5487@outlook.com>
2026-09-14 13:22:01 +0900

    slimenano-vfs: fix SHA512

--- ports/slimenano-vfs/portfile.cmake
+++ ports/slimenano-vfs/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Ren-Pen/slimenano-vfs
     REF "v${VERSION}"
-    SHA512 failing download because the expected SHA512 was all zeros, please change the expected SHA512 to: 96e06973e2d34846027efd32a8a4d73d1c24b8b64d2e60de4e80ab49bb73d81df3305896747c267a1e76bcc73844196aa76fa66a2fcf1abeeefc136f0479e2ba
+    SHA512 96e06973e2d34846027efd32a8a4d73d1c24b8b64d2e60de4e80ab49bb73d81df3305896747c267a1e76bcc73844196aa76fa66a2fcf1abeeefc136f0479e2ba
     HEAD_REF main
 )
 
